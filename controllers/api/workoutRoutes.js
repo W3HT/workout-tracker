22d3b8eae62dc238c2cff946f09778252ce3c45d
Frost <mpyang2021@gmail.com>
2021-11-12 19:28:26 -0600

    wireframed workoutRoutes.js

---
+++ controllers/api/workoutRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const Workouts = require('../../models/Workout');
+const mongojs = require('mongojs');
+
+// gest - last
+
+// post - new workout
+
+// gets - combined weight / total duration - limit 7 - aggregate
+
+// put - new exercise - /:id
+
+module.exports = router;
